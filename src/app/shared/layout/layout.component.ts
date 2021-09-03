7403fd1efa96dede3d1bc81bc4d2e79ff916a20c
Eugène Ndjaka <eugene.ndjaka@kaeyros-analytics.de>
2021-09-03 12:28:34 +0100

    Feat:starting header

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   templateUrl: 'layout.component.html',
+  styleUrls: ['layout.component.scss'],
   selector: 'app-layout'
 })
 export class LayoutComponent {
