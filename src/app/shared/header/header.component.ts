48cc8a71d8c68dbce8ccfaeb69a90f3d261d84f8
Eugène Ndjaka <eugene.ndjaka@kaeyros-analytics.de>
2021-09-03 11:17:01 +0100

    Feat:starting header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   templateUrl: 'header.component.html',
-  selector: 'app-header'
+  styleUrls: ['header.component.scss'],
+  selector: 'app-header',
 })
 export class HeaderComponent {
 
