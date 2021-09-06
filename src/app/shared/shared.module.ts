b51eecb9eac67723d914d0313d93448fac4aabcb
Eugène Ndjaka <eugene.ndjaka@kaeyros-analytics.de>
2021-09-06 09:07:16 +0100

    add link on the menu of navigation bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,10 @@ import {HeaderComponent} from './header/header.component';
 import {Circle} from './circle/circle';
 import {Rectangle} from './rectangle/rectangle';
 import {ProgramItem} from './program-item/program-item';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  imports: [ CommonModule ],
+    imports: [CommonModule, RouterModule],
   declarations: [ LayoutComponent, HeaderComponent, Circle, Rectangle, ProgramItem ],
   exports: [ LayoutComponent, HeaderComponent, Circle, Rectangle, ProgramItem ]
 })
