a60d64b70318a700a645c6b3703f0222307c344f
Eugène Ndjaka <eugene.ndjaka@kaeyros-analytics.de>
2021-09-09 21:04:45 +0100

    home page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,11 +6,12 @@ import {Circle} from './circle/circle';
 import {Rectangle} from './rectangle/rectangle';
 import {ProgramItem} from './program-item/program-item';
 import {RouterModule} from '@angular/router';
+import {CategoryComponent} from './category/category.component';
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
-  declarations: [ LayoutComponent, HeaderComponent, Circle, Rectangle, ProgramItem ],
-  exports: [ LayoutComponent, HeaderComponent, Circle, Rectangle, ProgramItem ]
+  imports: [CommonModule, RouterModule],
+  declarations: [LayoutComponent, HeaderComponent, Circle, Rectangle, ProgramItem, CategoryComponent],
+  exports: [LayoutComponent, HeaderComponent, Circle, Rectangle, ProgramItem, CategoryComponent]
 })
 export class SharedModule {
 
