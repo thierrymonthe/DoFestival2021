aaa75fbf44a1207fa4b0277a396e27fa99ab5263
yvan.kombou <yvan.kombou@gmail.com>
2021-09-13 02:33:54 +0200

    =update program page

--- src/app/pages/program/program.page.ts
+++ src/app/pages/program/program.page.ts
@@ -1,7 +1,8 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 
-interface ProgramItemDescription {
+export interface ProgramItemDescription {
   date: Date;
+  id?: string;
   stringDate?: string;
   stringTime?: string;
   imgSrc: string;
@@ -10,14 +11,24 @@ interface ProgramItemDescription {
   time?: Date;
   place?: string;
   link?: string;
+  routinkLing?: string;
   category?: {
     title: string;
     link: string;
+    more?: string;
   };
   details?: {
     tel: string;
     email: string;
     dienstag: string
+    title1?: string;
+    title2?: string;
+    content1?: string[];
+    content2?: string[];
+    other?: {
+      title?: string;
+      events?: {category: string, title: string, image: string}[]
+    };
   };
 }
 
@@ -34,6 +45,7 @@ export class ProgramPage {
     {
       place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund ',
       link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
+      routinkLing: '/program-detail/1',
       date: new Date(2021, 10, 5, 20),
       stringDate: 'Di 5 Okt 2021',
       stringTime: '20:00',
@@ -54,6 +66,7 @@ export class ProgramPage {
       place: 'Dortmunder U/Flux Flax, Leonie-Reygers-Terrasse 44137 Dortmund  ',
       link: 'https://www.google.de/maps/place/Dortmunder+U+Zentrum+f%C3%BCr+Kunst+und+Kreativit%C3%A4t/@51.5150033,7.4511446,17z/data=!4m12!1m6!3m5!1s0x47b919e8a09192e5:0xd1407c3198ff0b70!2sDortmunder+U+Zentrum+f%C3%BCr+Kunst+und+Kreativit%C3%A4t!8m2!3d51.515!4d7.4533333!3m4!1s0x47b919e8a09192e5:0xd1407c3198ff0b70!8m2!3d51.515!4d7.4533333',
       date: new Date(2021, 10, 8, 18, 30),
+      routinkLing: '/program-detail/2',
       stringDate: 'Fr 8 Okt 2021',
       imgSrc: './assets/themenfotos/_DTF0739.jpg',
       category: {
@@ -73,6 +86,7 @@ export class ProgramPage {
       place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund ',
       link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
       date: new Date(2021, 10, 5, 20),
+      routinkLing: '/program-detail/3',
       stringDate: 'Di 19 Okt 2021',
       imgSrc: './assets/themenfotos/_DTF0739.jpg',
       category: {
@@ -91,6 +105,7 @@ export class ProgramPage {
       place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund ',
       link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
       date: new Date(2021, 10, 5, 19),
+      routinkLing: '/program-detail/4',
       stringDate: 'Mo 22 Okt 2021',
       imgSrc: './assets/themenfotos/_DTF0739.jpg',
       category: {
@@ -109,6 +124,7 @@ export class ProgramPage {
       place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund ',
       link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
       date: new Date(2021, 10, 5, 19),
+      routinkLing: '/program-detail/5',
       stringDate: 'Fr 26 Okt 2021',
       imgSrc: './assets/themenfotos/_DTF0739.jpg',
       category: {
@@ -127,6 +143,7 @@ export class ProgramPage {
       place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund ',
       link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
       date: new Date(2021, 10, 5, 14),
+      routinkLing: '/program-detail/6',
       stringDate: 'Mo 29 Okt 2021',
       imgSrc: './assets/themenfotos/_DTF0739.jpg',
       category: {
@@ -145,6 +162,7 @@ export class ProgramPage {
       place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund ',
       link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
       date: new Date(2021, 10, 5, 20),
+      routinkLing: '/program-detail/7',
       stringDate: 'Di 5 Okt 2021',
       imgSrc: './assets/themenfotos/_DTF0739.jpg',
       category: {
@@ -163,6 +181,7 @@ export class ProgramPage {
       place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund ',
       link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
       date: new Date(2021, 10, 5, 20),
+      routinkLing: '/program-detail/8',
       stringDate: 'Di 5 Okt 2021',
       imgSrc: './assets/themenfotos/_DTF0739.jpg',
       category: {
@@ -181,6 +200,7 @@ export class ProgramPage {
       place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund ',
       link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
       date: new Date(2021, 10, 5, 19),
+      routinkLing: '/program-detail/9',
       stringDate: 'Fr 3 Nov 2021',
       imgSrc: './assets/themenfotos/_DTF0739.jpg',
       category: {
