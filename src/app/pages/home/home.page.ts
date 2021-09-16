0c149456edd15289a883d7bc047a7ddc12bb809f
Eugène Ndjaka <eugene.ndjaka@kaeyros-analytics.de>
2021-09-16 23:07:44 +0100

    home page responsive ending

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'] 
 })
 export class HomePage {
 
