c10ef3b090101c2895c679ff33314bac5da577ab
yvan.kombou <yvan.kombou@gmail.com>
2021-09-16 04:04:08 +0200

    =add 8 okt

--- src/app/pages/program/program.page.ts
+++ src/app/pages/program/program.page.ts
@@ -7,7 +7,8 @@ export interface ProgramItemDescription {
   stringTime?: string;
   imgSrc: string;
   content: string;
-  contentDetail?: {highlightStart?: number, highlightLength ?: number, link?: string};
+  contentDetail?: {text?: string, class ?: string, link?: string}[];
+  moreInformation?: string;
   title: string;
   time?: Date;
   place?: string;
@@ -65,13 +66,34 @@ export class ProgramPage {
       content: `Welche Rolle nimmt der Feminismus in einem Land ein, in dem es nur eine legale Frauenorganisation gibt? Dr. Sigrid Y. Palacios Castillo moderiert und wird dieser und weiterer Fragen im Gespräch den Abend mit Sandra Abd'Allah-Alvarez Ramírez, einer dekolonialen, antirassistischen und abolitionistischen kubanischen Afrofeministin nachgehen. `
     },
     {
+      id: 10,
+      place: 'Schauspiel Dortmund/Institut, Hilltropwall 15, 44137 Dortmund  ',
+      link: 'https://goo.gl/maps/GWNg4akPeB4jem1o8',
+      routinkLing: '/program-detail/10',
+      date: new Date(2021, 10, 12, 20),
+      stringDate: 'So 12 Okt 2021',
+      stringTime: '20:00',
+      imgSrc: './assets/themenfotos/2021_10_12_.png',
+      category: {
+        title: 'Sehen',
+        link: 'Schauspiel Dortmund/Institut'
+      },
+      details: {
+        tel: '0231/50 27 222',
+        email: 'telefonverkauf@theaterdo.de',
+        dienstag: 'Samstag: 10.00 – 18.30 Uhr'
+      },
+      title: 'Was bedeutet es, im Kuba des 21. Jahrhunderts Feministin zu sein?',
+      content: `Welche Rolle nimmt der Feminismus in einem Land ein, in dem es nur eine legale Frauenorganisation gibt? Dr. Sigrid Y. Palacios Castillo moderiert und wird dieser und weiterer Fragen im Gespräch den Abend mit Sandra Abd'Allah-Alvarez Ramírez, einer dekolonialen, antirassistischen und abolitionistischen kubanischen Afrofeministin nachgehen. `
+    },
+    {
       id: 2,
-      place: 'Dortmunder U/Flux Flax, Leonie-Reygers-Terrasse 44137 Dortmund  ',
+      place: 'Dortmunder U/Flux Flax, Leonie-Reygers-Terrasse 44137 Dortmund',
       link: 'https://www.google.de/maps/place/Dortmunder+U+Zentrum+f%C3%BCr+Kunst+und+Kreativit%C3%A4t/@51.5150033,7.4511446,17z/data=!4m12!1m6!3m5!1s0x47b919e8a09192e5:0xd1407c3198ff0b70!2sDortmunder+U+Zentrum+f%C3%BCr+Kunst+und+Kreativit%C3%A4t!8m2!3d51.515!4d7.4533333!3m4!1s0x47b919e8a09192e5:0xd1407c3198ff0b70!8m2!3d51.515!4d7.4533333',
       date: new Date(2021, 10, 8, 18, 30),
       routinkLing: '/program-detail/2',
       stringDate: 'Fr 8 Okt 2021',
-      imgSrc: './assets/themenfotos/_DTF0739.jpg',
+      imgSrc: './assets/program/Grup_foto3.JPG',
       category: {
         title: 'Begegnung',
         link: 'Dortmunder U: Flux Inn'
@@ -82,7 +104,7 @@ export class ProgramPage {
         email: 'mail@nullpluseinsfestival.de ',
         dienstag: 'Samstag: 10.00 – 18.30 Uhr'
       },
-      title: 'Was ist Männlichkeit heute? Ein Gespräch mit Malcolm Ohanwe, Mudjacka Mvunuku und Luka Timm\n',
+      title: 'Demokratiekunstwerk des Bildungswerk Vielfalt im Dortmunder U und Expert*innentalk zu der Frage: Welche Zukunftsfragen brauchen wir?',
       content: `Das vom Bildungswerk Vielfalt gestaltete ‚Demokratiekunstwerk‘ bietet keine Antwort auf die Fragen: Wo findet Zukunft statt? Wer wird am Zukunftsprozess beteiligt? Wer gestaltet Zukunft? es soll eine Diskurs eröffnen. Roxanna-Lorraine Witt spricht mit Prof. Dr. Yüksel Ekinci, Cesaire Sielatchom und Ulrike Podhajsky über die Entstehung des Kunstwerks und die diskutiert über die Fragen. `
     },
     {
