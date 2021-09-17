b35b29f321c5b53ce0c0c72a69d8d60ee9779875
chendjou2016 <chendjou2016@outlook.fr>
2021-09-17 16:35:00 +0100

    fix image home page

--- src/app/pages/home-image/home-image.page.ts
+++ src/app/pages/home-image/home-image.page.ts
@@ -1,8 +1,20 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   templateUrl: 'home-image.page.html',
   styleUrls: ['home-image.page.scss']
 })
-export class HomeImagePage {
+export class HomeImagePage implements OnInit {
+
+  @ViewChild('image')
+  image: ElementRef<HTMLElement>;
+
+  height: number = 0;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.height = this.image?.nativeElement.getBoundingClientRect().height;
+    }, 100);
+  }
+
 }
